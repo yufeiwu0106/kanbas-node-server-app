ba4e4024aec04643bc92a1523e1f2f289f4fd3fd
Gray Wu <graywu@Weirans-MacBook-Air.local>
2024-11-11 22:08:11 -0800

    finish 3.4

--- Lab5/index.js
+++ Lab5/index.js
@@ -2,15 +2,16 @@ import PathParameters from "./PathParameters.js";
 import QueryParameters from "./QueryParameters.js";
 import WorkingWithObjects from "./WorkingWithObjects.js";
 import Module from "./Module.js";
+import WorkingWithArrays from "./WorkingWithArrays.js";
 
 export default function Lab5(app) {
-    app.get("/lab5/welcome", (req, res) => {
-      res.send("Welcome to Lab 5");
-    });
+  app.get("/lab5/welcome", (req, res) => {
+    res.send("Welcome to Lab 5");
+  });
 
-    PathParameters(app);
-    QueryParameters(app);
-    WorkingWithObjects(app);
-    Module(app);
-  };
-  
+  PathParameters(app);
+  QueryParameters(app);
+  WorkingWithObjects(app);
+  Module(app);
+  WorkingWithArrays(app);
+}
